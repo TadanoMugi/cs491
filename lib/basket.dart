a3ec9091106c3e417a3c0dab0b626fd42ff5bde0
Chris <topheryun@gmail.com>
2020-02-17 17:16:57 -0800

    added basket

---
+++ lib/basket.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+import 'routing_constants.dart';
+
+// List<String> basket = new List<String>();
+List<String> basket = ['chocolate', 'milk', 'sugar', 'flour', 
+//  'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test'
+  ];
+
+Widget buildBasket(BuildContext context) {
+  return new AlertDialog(
+    title: const Text(
+      'Basket',
+      textAlign: TextAlign.center,
+    ),
+    content: new Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        // padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+        RichText(
+          text: new TextSpan(
+            text: 'Tap an ingredient to remove it from the basket\n',
+            style: const TextStyle(color: Colors.black87),
+          )
+        ),
+        Expanded(
+          child: new MyBasket()
+        ),
+        
+      ],
+      
+    ),
+    actions: <Widget>[
+      new Row(
+        children: <Widget>[
+          new RaisedButton(
+            color: Colors.grey[100],
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text(
+              'Go back',
+              style: TextStyle(
+                color: Colors.red,
+              )
+            )
+          ),
+          new SizedBox(width: 20,),
+          new RaisedButton(
+            color: Colors.grey[100],
+            onPressed: () {
+              Navigator.pushNamed(context, ResultPageRoute);
+            },
+            textColor: Theme.of(context).primaryColor,
+            child: const Text('Find me a recipe!'),
+          ),
+          new SizedBox(width: 10,),
+        ],
+      ),
+
+      
+    ],
+    
+  );
+}
+
+/* ==================================================
+// dynamic basket list that can be removed
+================================================== */
+class MyBasket extends StatefulWidget {
+  @override
+  _MyBasketState createState() => _MyBasketState();
+}
+class _MyBasketState extends State<MyBasket> {
+  List<String> list = basket;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: ListView.builder(
+      itemCount: list.length,
+      itemBuilder: (context, index) {
+        return MyItem(list[index], onDelete: () => removeItem(index));
+      },
+      )
+    );
+  }
+
+/* ==================================================
+// function for removing items in basket/list
+================================================== */
+  void removeItem(int index) {
+    setState(() {
+      list = List.from(list)
+        ..removeAt(index);
+    });
+    basket = List.from(basket)
+        ..removeAt(index);
+  }
+}
+
+/* ==================================================
+// this creates each "card" in basket
+================================================== */
+class MyItem extends StatelessWidget {
+  final String title;
+  final VoidCallback onDelete;
+
+  MyItem(this.title, {this.onDelete});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(this.title),
+        onTap: this.onDelete,
+      )
+    );
+  }
+}
--- lib/search_page.dart
+++ lib/search_page.dart
@@ -17,21 +17,25 @@ GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
       body: ListView(
         padding: EdgeInsets.all(10.0),
         children: letters
-            .map((data) => ListTile(
-                  leading: Icon(null),
-                  title: Text(data),
-                  onTap: () => _scaffoldKey.currentState.showSnackBar(SnackBar(
-                        content: Text("$data added!"),
-                        duration: Duration(seconds: 1),
-                      )),
-                ))
-            .toList(),
+          .map((data) => ListTile(
+                leading: Icon(null),
+                title: Text(data),
+                onTap: () => _scaffoldKey.currentState.showSnackBar(SnackBar(
+                      content: Text("$data added!"),
+                      duration: Duration(seconds: 1),
+// @terrance, figure out how to add these to basket from basket.dart
+                    )),
+              ))
+          .toList(),
       ),
       floatingActionButton: FloatingActionButton(
         elevation: 10.0,
-        child: Icon(Icons.fastfood),
+        child: Icon(Icons.shopping_basket),
         onPressed: (){
-          Navigator.pushNamed(context, ResultPageRoute);
+          showDialog(
+            context: context,
+            builder: (BuildContext context) => buildBasket(context),
+          );
         },
       ),
     );
